17e39e9a31318a5f7f3be61025417fdc360ac103
Apoorva Aditya <apoorvaufo3@gmail.com>
2022-02-25 23:22:33 -0600

    added home and away pages

--- flask/app.py
+++ flask/app.py
@@ -21,6 +21,14 @@ def gen_frames():
 def index():
     return render_template('index.html')
 
+@app.route('/home', methods=['POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/away', methods=['POST'])
+def away():
+    return render_template('away.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
