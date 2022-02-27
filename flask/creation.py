3f915e78b0408d0e4990578ce158a1ae82bb102a
Ribhavs2 <ribhavs2@illinois.edu>
2022-02-26 23:02:16 -0600

    Training

--- flask/creation.py
+++ flask/creation.py
@@ -2,14 +2,16 @@ import cv2, numpy, sys, os
 
 datasets = 'datasets'
 
-user = input('\nEnter your name and press <return> ==>')
+
+#user = input('\nEnter your name and press <return> ==>')
+face_id = input('\n enter user id end press <return> ==>  ')
 
 
-path_face = os.path.join(datasets,user)
+#path_face = os.path.join(datasets,user)
 #path_smile = os.path.join(datasets,user,'smile')
 #path_eye = os.path.join(datasets,user,'eye')
 
-os.mkdir(path_face)
+#os.mkdir(path_face)
 #os.mkdir(path_smile)
 #os.mkdir(path_eye)
 
@@ -23,7 +25,7 @@ minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)                                                                           
 count = 0
 
-while count <= 50:
+while count <= 300:
     rect, pic = cam.read()
     pic = cv2.flip(pic,1)
     gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
@@ -37,7 +39,7 @@ while count <= 50:
     for (x, y, w, h) in faces: # (x,y) -> top left corner coordinate, w -> width, h -> height
         cv2.rectangle(pic, (x, y), (x+w, y+h), (255, 0, 0), 2)
         img_g = gray[y:y+h, x:x+w]
-        cv2.imwrite('% s/% s.png' % (path_face,count), img_g)
+        cv2.imwrite("datasets/User." + str(face_id) + '.' + str(count) + ".jpg",img_g)
     
     cv2.imshow('video', pic)
     count = count + 1
